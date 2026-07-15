e2f13e9a14992722386ebcf1b6d779b9f4a390fe
Matt Reynolds <matt.j.reynolds@iinet.net.au>
2026-07-15 17:45:20 +1000

    Add ScrollToTop component and integrate it into AppRouter for improved navigation experience

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,16 +4,21 @@ import About from '../pages/About';
 import Projects from '../pages/Projects';
 import Skills from '../pages/Skills';
 import Contact from '../pages/Contact';
+import ScrollToTop from '../utils/ScrollToTop';
 
 function AppRouter() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='projects' element={<Projects />} />
-      <Route path='skills' element={<Skills />} />
-      <Route path='contact' element={<Contact />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='projects' element={<Projects />} />
+        <Route path='skills' element={<Skills />} />
+        <Route path='contact' element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
